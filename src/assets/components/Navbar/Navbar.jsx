30e8cf361bbc7bff6ad8c48586b175b6bda699ed
SahanDev00 <dumidus86@gmail.com>
2024-01-18 19:35:49 +0530

    Commited

--- src/assets/components/Navbar/Navbar.jsx
+++ src/assets/components/Navbar/Navbar.jsx
@@ -3,8 +3,29 @@ import React from 'react'
 const Navbar = () => {
     return (
         <>
-          <div className='shadow-md bg-white'>
-              <div>Hello</div>
+          <div className='shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200'>
+              <div className='container'>
+                    <div className="flex justify-center items-center">
+                        <div>
+                            <a href="#">
+                                <img src={} alt="Foodie Zone" className='w-10'/> Foodie
+                            </a>
+                        </div>
+                        <div>
+                            <ul>
+                                <li>
+                                    <a href=""></a>
+                                </li>
+                                <li>
+                                    <a href=""></a>
+                                </li>
+                                <li>
+                                    <a href=""></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+              </div>
           </div>  
         </>
     )
