abd419e1a092da999568bf7c87183001cdf8984f
SahanDev00 <dumidus86@gmail.com>
2024-01-20 23:20:01 +0530

    Navbar

--- src/assets/components/Navbar/Navbar.jsx
+++ src/assets/components/Navbar/Navbar.jsx
@@ -1,28 +1,33 @@
 import React from 'react'
+import {FaShoppingCart} from 'react-icons/fa'
 
 const Navbar = () => {
     return (
         <>
           <div className='shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200'>
-              <div className='container'>
-                    <div className="flex justify-center items-center">
+              <div className='container py-3 sm:py-0'>
+                    <div className="flex justify-between items-center">
                         <div>
-                            <a href="#">
-                                <img src={} alt="Foodie Zone" className='w-10'/> Foodie
+                            <a href="#" className='flex items-center gap-2 text-2xl sm:text-3xl font-bold'>
+                                <img src='' alt="Foodie Zone" className='w-10'/> Foodie
                             </a>
                         </div>
                         <div>
-                            <ul>
+                            <ul className='hidden sm:flex gap-4'>
                                 <li>
-                                    <a href=""></a>
+                                    <a href="#" className='inline-block p-4 hover:text-primary'>Home</a>
                                 </li>
                                 <li>
-                                    <a href=""></a>
+                                    <a href="#" className='inline-block p-4 hover:text-primary'>About</a>
                                 </li>
                                 <li>
-                                    <a href=""></a>
+                                    <a href="#" className='inline-block p-4 hover:text-primary'>Contact</a>
                                 </li>
                             </ul>
+                            <button className='bg-gradient-to-r from-primary to-secondary text-white px-4 py-1 rounded-full hover:scale-105 duration-300 flex items-center gap-2'>
+                                Order
+                                <FaShoppingCart className='text-xl text-white drop-shadow-sm cursor-pointer' />
+                            </button>
                         </div>
                     </div>
               </div>
