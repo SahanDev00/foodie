960f6bb19ec3e81c71dcb6ddde1b8cd7d4ac4154
SahanDev00 <dumidus86@gmail.com>
2024-01-23 21:46:33 +0530

    Commited

--- src/assets/components/Hero/Hero.jsx
+++ src/assets/components/Hero/Hero.jsx
@@ -36,11 +36,28 @@ const Hero = () => {
                 <div className='container pb-8 sm:pb-0'>
                     <div className='grid grid-cols-1 sm:grid-cols-2'>
                         <div className='flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1'>
-                            <h1 className='text-5xl sm:text-6xl lg:text-7xl font-bold'>Welcome to Foodie</h1>
+                            <h1 className='text-5xl sm:text-6xl lg:text-7xl font-bold'>Welcome to <span className=' sm:text-secondary'>Foodie</span></h1>
                             <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, magni. Soluta, facilis. Voluptas dolorem cupiditate sed doloremque neque, corporis officiis?</p>
+                            <div>
+                                <button className='bg-gradient-to-r from-primary to-secondary text-white px-4 py-2 rounded-full hover:scale-105 duration-200'>Order Now</button>
+                            </div>
                         </div>
-                        <div>
-
+                        <div className='order-1 sm:order-2 min-h-[450px] sm:min-h-[450px] flex justify-center items-center relative'>
+                            <div className='flex justify-center items-center h-[300px] sm:h-[450px] overflow-hidden'>
+                                <img src={imageId} alt="" className='w-[300px] sm:w-[450px] mx-auto spin' />
+                            </div>
+                            <div className='flex lg:flex-col lg:top-1/2 lg:-translate-y-1/2 lg:py-2 justify-center gap-4 absolute bottom-[0px] lg:-right-10 bg-white/30 rounded-full'>
+                                {
+                                    ImgList.map((item) => (
+                                        <img key={item.id} src={item.img} alt="" className='max-w-[80px] h-[80px] object-contain inline-block hover:scale-105 duration-200' 
+                                        onClick={() => {
+                                            setImageId(
+                                                item.id === 1 ? Food1 : item.id === 2 ? Food2 : Food3
+                                            );
+                                        } }/>
+                                    ))
+                                }
+                            </div>
                         </div>
                     </div>
                 </div>
