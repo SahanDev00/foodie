0387b449ed662cf6c927d7a1d4abf12711fae5eb
SahanDev00 <dumidus86@gmail.com>
2024-01-23 21:46:20 +0530

    Commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './assets/components/Hero/Hero'
 import Navbar from './assets/components/Navbar/Navbar'
+import Services from './assets/components/Services/Services'
 
 const App = () => {
   return (
     <>
       <Navbar/>
       <Hero/>
+      <Services/>
     </>
   )
 }
