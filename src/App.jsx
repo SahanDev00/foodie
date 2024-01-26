128779e9c4c554ab2dc4fdf6470717893126c786
SahanDev00 <dumidus86@gmail.com>
2024-01-26 20:12:08 +0530

    Commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Banner from './assets/components/Banner/Banner'
 import Hero from './assets/components/Hero/Hero'
 import Navbar from './assets/components/Navbar/Navbar'
 import Services from './assets/components/Services/Services'
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <Services/>
+      <Banner/>
     </>
   )
 }
