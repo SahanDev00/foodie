d24bdc23cd90a48452af46ff1fb5296972ad305c
SahanDev00 <dumidus86@gmail.com>
2024-01-26 20:12:25 +0530

    Commited

--- src/assets/components/Services/Services.jsx
+++ src/assets/components/Services/Services.jsx
@@ -25,11 +25,11 @@ const ServicesData = [
 const Services = () => {
     return (
         <>
-            <div className='py-10'>
-                <div className='container'>
+            <div className='py-10 dark:bg-gray-900'>
+                <div className='container '>
                     <div className='text-center mb-20 max-w-[400px] mx-auto'>
-                        <p className='text-sm bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary'>Our Services</p>
-                        <h1 className='text-3xl font-bold'>Services</h1>
+                        <p className='text-md bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary'>Our Services</p>
+                        <h1 className='text-3xl font-bold dark:text-white pb-2'>Services</h1>
                         <p className='text-xs text-gray-400'>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus vitae deserunt dignissimos obcaecati rerum quo laudantium officia reiciendis qui aliquam.
                         </p>
@@ -39,13 +39,13 @@ const Services = () => {
                         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-14 md:gap-5 place-items-center'>
                             {ServicesData.map(({id, img, name, description}) => {
                                 return (
-                                    <div key={id} className='max-w-[300px] group rounded-2xl bg-white dark:bg-gray-800 dark:hover:bg-primary hover:text-white duration-300 shadow-xl py-5'>
+                                    <div key={id} className='max-w-[300px] group rounded-2xl bg-white dark:bg-gray-800 dark:hover:bg-primary  duration-300 shadow-xl py-5'>
                                         <div className='h-[100px]'>
                                             <img src={img} alt="" className='max-w-[200px] mx-auto block transform -translate-y-14 group-hover:scale-105 group-hover:rotate-6 duration-300' />
                                         </div>
                                         <div className='p-4 text-center'>
-                                            <h1 className='text-xl font-bold'>{name}</h1>
-                                            <p className='text-gray-500 group-hover:text-white duration-300 text-sm line-clamp-2'>{description}</p>
+                                            <h1 className='text-xl font-bold dark:text-white'>{name}</h1>
+                                            <p className='text-gray-500  duration-300 text-sm line-clamp-2'>{description}</p>
                                         </div>
                                     </div>
                                 )
